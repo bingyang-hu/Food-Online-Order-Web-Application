b304e178ecbabc6f8dfae04e8c2938398975546e
bingyang-hu <zoey4y@gmail.com>
2022-06-22 19:02:56 -0700

    MVC

--- onlineOrder/src/main/java/com/example/onlineorder/HelloServlet.java
+++ onlineOrder/src/main/java/com/example/onlineorder/HelloServlet.java
@@ -1,8 +1,15 @@
 package com.example.onlineorder;
+import com.example.onlineorder.entity.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+
+import org.apache.commons.io.IOUtils; // get raw data and convert it to json
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -13,15 +20,33 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+        response.setContentType("application/json");// 返回数据类型： json
+    ObjectMapper mapper = new ObjectMapper();
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+    Customer customer = new Customer();
+
+    customer.setFirstName("Rick");
+    customer.setLastName("Sun");
+    response.getWriter().println(mapper.writeValueAsString(customer));
+//
     }
 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        JSONObject obj = new JSONObject(IOUtils.toString(request.getReader())); // 读取request body data
+
+        String lastName= obj.getString("last_name");
+        int age = obj.getInt("age");
+        String email = obj.getString("email");
+
+        System.out.println("Email is: " + email);
+        System.out.println("Age is: " + age);
+        System.out.println("Last name is: " + lastName);
+
+
+
+
+
+    }
     public void destroy() {
     }
 }
